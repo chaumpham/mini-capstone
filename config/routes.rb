fd83684db5fcabf0ee9ede3b3b640dbfb7835010
Chau Pham <theonlychau@gmail.com>
2017-11-14 20:47:17 -0600

    version1 modified per Peter's codes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   namespace :v1 do
-    get"/mini_capstone_url" => "products#product_method"
+    get"/all_products_url" => "products#all_products_method"
+    get"/one_product_url" => "products#one_product_method"
   end
 
   # namespace :v2 do
