90f2c4a051a3b28a36e8fc21e9a4c99ae21373b2
Chau Pham <theonlychau@gmail.com>
2017-11-14 18:52:20 -0600

    modified seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+product = product.new({name:"cat sitter", price:"$25", image:"http://101petsitting.com/wp-content/uploads/2012/11/CatsLotsLady.jpg", description:"feed your cat, clean up your cat's waste, change your cat's litter, play with your cat"})
+product.save
+
+product = product.new({name:"dog walker", price:"$15", image:"http://www.noobpreneur.com/wp-content/uploads/2016/10/dog-walker.jpg", description:"walk your dog"})
+product.save
+
+product = product.new({name:"baby sitter", price:"$10", image:"https://jokideo.com/wp-content/uploads/2013/08/Creepy-meme-The-new-babysitter.png", description:"can babysit your evilest child"})
+product.save
+
+product = product.new({name:"pretend to be your girlfriend for a day", price:"$255", image:"https://pics.me.me/me-as-a-girlfriend-woutre-blocking-myviewg-21031624.png", description:"price not including dinner, pickup/drop-off, tips, gifts, clothes, anything else"})
+product.save
