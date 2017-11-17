65148b4a57b06f497a96c2ad803a5c02581868fc
Chau Pham <theonlychau@gmail.com>
2017-11-16 19:51:32 -0600

    made second change to RESTful create action

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   namespace :v1 do
     get "/products" => "products#index"
-    get"/products" => "products#create"
-    get"/products/:id" => "products#show"
+    post "/products" => "products#create"
+    get "/products/:id" => "products#show"
   end
 
   # namespace :v2 do
