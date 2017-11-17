81fbfbd1ffdb5fd2c1760353767ee58986e61412
Chau Pham <theonlychau@gmail.com>
2017-11-16 18:49:57 -0600

    add RESTful create action

--- app/controllers/v1/products_controller.rb
+++ app/controllers/v1/products_controller.rb
@@ -4,6 +4,18 @@ class V1::ProductsController < ApplicationController
     render json: products.as_json
   end
 
+  def create
+    prodcut = Product.new(
+      name: params["input_name"],
+      price: params["input_price"],
+      image: params["input_image"],
+      description: params["input_image"]
+      )
+
+    product.save
+    render json: product.as_json
+  end
+
   def show
     product_id = params["id"]
     product = Product.find_by(id: product_id)
